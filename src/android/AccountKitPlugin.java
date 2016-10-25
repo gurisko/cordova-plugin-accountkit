6023eb8363e3d4d503d33328b6700052ceeccf8c
Matus Gura <matus.gura@gmail.com>
2016-10-25 20:11:09 +0200

    Handle events from the AccountKit activity on Android

--- src/android/AccountKitPlugin.java
+++ src/android/AccountKitPlugin.java
@@ -16,6 +16,7 @@ import org.json.JSONException;
 
 import com.facebook.accountkit.AccountKit;
 import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKitLoginResult;
 import com.facebook.accountkit.ui.AccountKitActivity;
 import com.facebook.accountkit.ui.AccountKitConfiguration;
 import com.facebook.accountkit.ui.LoginType;
@@ -23,7 +24,8 @@ import com.facebook.accountkit.ui.LoginType;
 
 public class AccountKitPlugin extends CordovaPlugin {
   private static final String TAG = "AccountKitPlugin";
-
+  public static int APP_REQUEST_CODE = 42;
+  private CallbackContext loginContext = null;
 
   @Override
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
@@ -34,53 +36,114 @@ public class AccountKitPlugin extends CordovaPlugin {
 
   @Override
   public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    if ("loginWithPhone".equals(action)) {
+    if ("loginWithPhoneNumber".equals(action)) {
       cordova.getActivity().runOnUiThread(new Runnable() {
         @Override
         public void run() {
-          login(args, LoginType.PHONE);
-          callbackContext.success();
+          executeLogin(LoginType.PHONE, callbackContext);
         }
       });
+      return true;
+
     } else if ("loginWithEmail".equals(action)) {
       cordova.getActivity().runOnUiThread(new Runnable() {
         @Override
         public void run() {
-          login(args, LoginType.EMAIL);
-          callbackContext.success();
+          executeLogin(LoginType.EMAIL, callbackContext);
         }
       });
+      return true;
+
+    } else if ("getAccessToken".equals(action)) {
+      if (hasAccessToken()) {
+        callbackContext.success(formatAccessToken(AccountKit.getCurrentAccessToken()));
+      } else {
+        callbackContext.error("Session not open.");
+      }
+      return true;
+
     } else if ("logout".equals(action)) {
       AccountKit.logOut();
-    } else {
-      callbackContext.error("Unknown Action: " + action);
-      return false;
+      callbackContext.success();
+      return true;
+
     }
-    return true;
+    return false;
   }
 
-  public final void login(JSONArray args, LoginType type) {
-    Log.d(TAG, "Login");
-    JSONObject parameters;
+  public final void executeLogin(LoginType type, CallbackContext callbackContext) {
+    // Set a pending callback to cordova
+    loginContext = callbackContext;
+    PluginResult pr = new PluginResult(PluginResult.Status.NO_RESULT);
+    pr.setKeepCallback(true);
+    loginContext.sendPluginResult(pr);
 
     try {
-      parameters = args.getJSONObject(0);
+      if (hasAccessToken()) {
+        callbackContext.success(formatAccessToken(AccountKit.getCurrentAccessToken()));
+        return;
+      }
     } catch (JSONException e) {
-      parameters = new JSONObject();
+      e.printStackTrace();
     }
 
-    // TODO: Add configuration
-
-    Context context = this.cordova.getActivity();
-    Intent intent = new Intent(context, AccountKitActivity.class);
+    Intent intent = new Intent(this.cordova.getActivity(), AccountKitActivity.class);
     AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
       new AccountKitConfiguration.AccountKitConfigurationBuilder(
         type,
-        AccountKitActivity.ResponseType.CODE);
-    intent.putExtra(
-      AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
-      configurationBuilder.build());
-    context.startActivity(intent);
+        AccountKitActivity.ResponseType.TOKEN);
+    intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configurationBuilder.build());
+
+    cordova.setActivityResultCallback(this);
+    cordova.startActivityForResult(this, intent, APP_REQUEST_CODE);
   }
 
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode != APP_REQUEST_CODE) {
+      return;
+    }
+
+    AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+    if (loginResult.getError() != null) {
+      loginContext.error(loginResult.getError().getErrorType().getMessage());
+
+    } else if (loginResult.wasCancelled()) {
+      loginContext.error("User cancelled dialog");
+
+    } else {
+      JSONObject result = null;
+
+      try {
+        final AccessToken accessToken = loginResult.getAccessToken();
+        if (accessToken != null) {
+          result = formatAccessToken(accessToken);
+        } else {
+          result = new JSONObject();
+          result.put("code", loginResult.getAuthorizationCode());
+          result.put("state", loginResult.getFinalAuthorizationState());
+        }
+        loginContext.success(result);
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+    }
+    loginContext = null;
+  }
+
+  private boolean hasAccessToken() {
+    return AccountKit.getCurrentAccessToken() != null;
+  }
+
+  public JSONObject formatAccessToken(AccessToken accessToken) throws JSONException {
+    JSONObject result = new JSONObject();
+    result.put("accountId", accessToken.getAccountId());
+    result.put("applicationId", accessToken.getApplicationId());
+    result.put("token", accessToken.getToken());
+    result.put("lastRefresh", accessToken.getLastRefresh().getTime() * 1000);
+    result.put("refreshInterval", accessToken.getTokenRefreshIntervalSeconds());
+    return result;
+  }
 }
